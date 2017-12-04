05a62077c6972498c258149ba37fbd572567396f
theBufferOverflow <34210347+theBufferOverflow@users.noreply.github.com>
2017-12-04 12:07:30 +0000

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,18 @@
+# CODEBY Python Challenge
+# First task
+# Author: ETERN4L
+
+def main():
+    try:
+        digit = int(input("Please, input a integer: "))
+        if digit % 3 == 0:
+            print("True.")
+        else:
+            print("False.")
+    except:
+        print("Unexpected error! Please, try again.")
+    finally:
+        input()
+
+if __name__ == "__main__":
+    main()
